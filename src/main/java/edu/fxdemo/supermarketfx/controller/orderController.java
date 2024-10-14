e35b6a79f860498efb8b57ac2fd75058b4d1fc36
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-14 18:02:32 +0530

    Orders Ui

--- src/main/java/edu/fxdemo/supermarketfx/controller/orderController.java
+++ src/main/java/edu/fxdemo/supermarketfx/controller/orderController.java
@@ -1,6 +1,86 @@
 package edu.fxdemo.supermarketfx.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class orderController {
+
+    @FXML
+    private ComboBox<?> cmbCustomerId;
+
+    @FXML
+    private ComboBox<?> cmbItemId;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colItemId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private Label lblCustomerName;
+
+    @FXML
+    private Label lblItemName;
+
+    @FXML
+    private Label lblItemPrice;
+
+    @FXML
+    private Label lblItemQty;
+
+    @FXML
+    private Label lblOrderId;
+
+    @FXML
+    private Label orderDate;
+
+    @FXML
+    private TableView<?> tblCart;
+
+    @FXML
+    private TextField txtAddToCartQty;
+
+    @FXML
+    void btnAddToCartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResetOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbItemOnAction(ActionEvent event) {
+
+    }
 
 }
